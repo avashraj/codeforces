92fdd7a5fa93c80815e1ad6491bf38a8fe00a69c
Avash Adhikari <avashraj328@gmail.com>
2024-10-29 12:14:08 -0700

    batman

---
+++ 4a_watermelon.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int w;
+    std::cin >> w;
+        
+    if (w == 2) {
+        std::cout << "NO" << std::endl;
+        return 0;
+    }
+
+    if (w % 2 == 0) {
+        std::cout << "YES" << std::endl;
+    } else {
+        std::cout << "NO" << std::endl;
+    }
+    return 0;
+}
